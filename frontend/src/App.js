9a7fcf19471a9729d573cd2847dd13c6bf609cab
Narindra69 <narindrakoko@gmail.com>
2024-08-09 22:44:11 +0300

    ajout du frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSection from './HeroSection';
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
